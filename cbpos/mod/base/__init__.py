f9469f07f0e80dd7a2b23da8c0eb29606e43ea7a
jadkik <jadkik94@gmail.com>
2013-01-24 03:59:19 +0200

    Change menu/actions definitions according to changes in core

--- cbpos/mod/base/__init__.py
+++ cbpos/mod/base/__init__.py
@@ -1,18 +1,54 @@
+from pydispatch import dispatcher
+
 import cbpos
 from cbpos.modules import BaseModuleLoader
 
 class ModuleLoader(BaseModuleLoader):
     name = 'Base Module'
 
+    def init(self):
+        dispatcher.connect(cbpos.terminate, signal='exit', sender=dispatcher.Any)
+        
+        return True
+
     def ui_handler(self):
         from cbpos.mod.base.ui import QtUIHandler
         return QtUIHandler()
 
     def menu(self):
-        return [[{'label': 'Main', 'rel': 0, 'priority':5, 'image': cbpos.res.base('images/menu-main.png')},
-                 {'label': 'System', 'rel': -1, 'priority':4, 'image': cbpos.res.base('images/menu-system.png')},
-                 {'label': 'Administration', 'rel': -1, 'priority': 5, 'image': cbpos.res.base('images/menu-administration.png')}],
-                []]
+        from cbpos.interface import MenuRoot
+        return [
+                [MenuRoot('main',
+                            label=cbpos.tr.base._('Main'),
+                            icon=cbpos.res.base('images/menu-main.png'),
+                            rel=0,
+                            priority=5
+                          ),
+                 MenuRoot('system',
+                            label=cbpos.tr.base._('System'),
+                            icon=cbpos.res.base('images/menu-system.png'),
+                            rel=-1,
+                            priority=4
+                          ),
+                 MenuRoot('administration',
+                            label=cbpos.tr.base._('Administration'),
+                            icon=cbpos.res.base('images/menu-administration.png'),
+                            rel=-1,
+                            priority=5
+                          )
+                 ],
+                []
+                ]
+    
+    def actions(self):
+        from cbpos.interface import Action
+        return [Action('quit',
+                       label=cbpos.tr.base._('Exit'),
+                       icon=cbpos.res.base("images/cancel.png"),
+                       shortcut='Ctrl+Q',
+                       signal='exit'
+                       )
+                ]
 
     def config_pages(self):
         from cbpos.mod.base.views import MenuConfigPage, AppConfigPage, LocaleConfigPage
