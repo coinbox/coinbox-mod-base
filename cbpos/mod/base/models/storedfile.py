0072810dbe6a1d24f06737f40422c08ff1e7321b
Jad Kik <jadkik94@gmail.com>
2013-08-03 21:44:32 +0300

    Added error handling to StoredFile model

--- cbpos/mod/base/models/storedfile.py
+++ cbpos/mod/base/models/storedfile.py
@@ -1,4 +1,6 @@
-import os
+import tempfile, os
+from cStringIO import StringIO
+from PIL import Image
 
 import cbpos
 
@@ -18,25 +20,40 @@ class StoredFile(cbpos.database.Base, Item):
     filetype = Column(String(5), default='')
     content = deferred(Column("content", LargeBinary(2**32-1)))
 
-    def __init__(self, filename):
-        basename = os.path.basename(filename)
-        dummy, ext = os.path.splitext(basename)
+    def __init__(self, path, content_file=None):
+        basename = os.path.basename(path)
+        filename, filetype = os.path.splitext(basename)
         # TODO: guess file type from mimetypes package, not extension
-        filetype = ext[1:]
-        with open(filename, 'rb') as f:
-            content = f.read()
+        if content_file is None:
+            try:
+                with open(path, 'rb') as f:
+                    content = f.read()
+            except IOError as e:
+                logger.error("Could not read file")
+                logger.error(e)
+                content = u""
+        elif isinstance(content_file, basestring):
+            content = content_file
+        else:
+            content_file.seek(0)
+            content = content_file.read()
         
-        super(StoredFile, self).__init__(filetype=filetype, content=content, filename=basename)
+        super(StoredFile, self).__init__(filetype=filetype, content=content, filename=filename)
 
     @classmethod
     def image(cls, path, size=None, format=None):
         
         # First, open the path
-        # TODO: what happens if the file is not an image? It should raise a meaningful exception
-        #         Maybe ValueError: Invalid image path / File does not exist / etc. Could be shown to the user
+        try:
+            im = Image.open(path)
+        except IOError:
+            # PIL cannot identify the image
+            # Or the image is not readable
+            raise ValueError("Invalid image file")
         
-        from PIL import Image
-        im = Image.open(path)
+        # Get some info about it
+        basename = os.path.basename(path)
+        filename, ext = os.path.splitext(basename)
         original_size = im.size
         
         if size is None and format is None:
@@ -44,39 +61,84 @@ class StoredFile(cbpos.database.Base, Item):
             return cls(path)
         
         if format is None:
-            basename = os.path.basename(filename)
-            dummy, ext = os.path.splitext(basename)
-            # TODO: guess file type from mimetypes package, not extension
-            format = ext
+            # If no specific format is specified just use whatever is there
+            # And keep the same extension
+            PIL_format = im.format
+            format = ext[1:] if ext.startswith(".") else ext
+        else:
+            # Let PIL guess it from the format (extension)
+            PIL_format = None
         
         if size is None:
             # Do not resize
             # Note: do not return because format may be different (transparency, background, etc.)
             size = original_size
+        else:
+            size = tuple(size)
+        
+        logger.debug("Resizing from %s to %s", original_size, size)
+        logger.debug("Converting to %s/%s from %s/%s", format, PIL_format, ext, im.format)
         
         # If some processing is necessary, do it now.
-        
-        import tempfile, os
-        
-        # Create a temporary file
-        fd, temp = tempfile.mkstemp(suffix="."+format)
-        
-        logger.debug("Image will be saved temporarily to: " + temp)
-        
-        # Resize the image
         final_im = Image.new("RGB", size, "white")
+        # Resize the image
         im.thumbnail(size, Image.ANTIALIAS)
         
+        # Center it in the image with the desired size
         actual_size = im.size
         paste_pos = tuple((size[i]-actual_size[i])/2 for i in (0, 1))
         final_im.paste(im, paste_pos)
-        final_im.save(temp)
         
-        # Create the StoredFile instance
-        image = cls(temp)
-        
-        # tempfile.mkstemp does nor delete the file once created
-        os.remove(temp)
+        try:
+            # Create a temporary file
+            fd, temp = tempfile.mkstemp(suffix="."+format)
+        except IOError as e:
+            logger.exception("Could not create temporary file. Manipulating in memory")
+            
+            f = StringIO()
+            # We cannot use the below because of cStringIO, it works for StringIO
+            #f.name = path
+            # So we try to guess it, just like PIL does it
+            if PIL_format is None:
+                try:
+                    PIL_format = Image.EXTENSION["."+format]
+                except KeyError:
+                    Image.init()
+                    try:
+                        PIL_format = Image.EXTENSION["."+format]
+                    except KeyError:
+                        # let it fail from within PIL's save
+                        pass
+                logger.debug("Final PIL_format: %s", PIL_format)
+            
+            try:
+                final_im.save(f, PIL_format)
+            except KeyError:
+                # PIL cannot identify the image
+                raise ValueError("Invalid output image type")
+            image = cls(filename+"."+format, f)
+            
+            f.close()
+        else:
+            logger.debug("Image will be saved temporarily to: " + temp)
+            
+            try:
+                final_im.save(temp, PIL_format)
+            except KeyError:
+                # PIL cannot identify the image
+                raise ValueError("Invalid output image type")
+            
+            with open(temp, 'rb') as f:
+                # Create the StoredFile instance
+                image = cls(filename+"."+format, f)
+            
+            # tempfile.mkstemp does not delete the file once created
+            try:
+                os.remove(temp)
+            except (OSError, IOError) as e:
+                # Could not delete the temp file
+                # Maybe because it's on Windows, or it's already gone
+                pass
     
         return image
 
@@ -98,7 +160,7 @@ class StoredFile(cbpos.database.Base, Item):
 
     def cached(self):
         # TODO: do not use resources, use data (userdata) for files which can be modified
-        return cbpos.res.base('cache/{}.{}'.format(self.id, self.filetype))
+        return cbpos.res.base('cache/{}{}'.format(self.id, self.filetype))
 
     def __repr__(self):
         return "<StoredFile %s>" % (self.filename,)
