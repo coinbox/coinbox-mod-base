bb5a3b612a015248c16a69413a7c0ecd962ad6c1
Jad Kik <jadkik94@gmail.com>
2013-09-28 22:25:12 +0300

    Update the UI handler and how windows are displayed

--- cbpos/mod/base/ui.py
+++ cbpos/mod/base/ui.py
@@ -1,6 +1,7 @@
 import PySide
 from PySide import QtCore, QtGui
 import sys
+from collections import defaultdict
 
 import cbpos
 logger = cbpos.get_logger(__name__)
@@ -8,25 +9,92 @@ logger = cbpos.get_logger(__name__)
 class QtUIHandler(cbpos.BaseUIHandler):
     application = None
     extensions = None
+    __started = False
     __window = None
     __do_load = True
+    __current_window = None
+    
+    PRIORITY_MIN = 0x00
+    
+    PRIORITY_FIRST = 0x10
+    PRIORITY_FIRST_LOW = 0x11
+    PRIORITY_FIRST_MEDIUM = 0x12
+    PRIORITY_FIRST_HIGH = 0x13
+    
+    PRIORITY_NONE = 0x20
+    
+    PRIORITY_LAST = 0x30
+    PRIORITY_LAST_LOW = 0x31
+    PRIORITY_LAST_MEDIUM = 0x32
+    PRIORITY_LAST_HIGH = 0x33
+    
+    PRIORITY_MAX = 0xFF
     
     def __init__(self):
         self.extensions = []
+        self.__window_chain = defaultdict(list)
     
     @property
-    def window(self):
+    def load(self):
+        return self.__do_load
+    
+    @load.setter
+    def load(self, val):
+        self.__do_load = bool(val)
+    
+    def set_main_window(self, win):
+        logger.debug("Setting main window to %s", win)
+        self.__window = win
         return self.__window
     
-    @window.setter
-    def window(self, val):
-        self.__do_load = True
-        self.__window = val
+    def replace_window(self, win):
+        logger.debug("Replacing window with %s", win)
+        if not self.__started:
+            raise ValueError("Application not started. Cannot replace window.")
+        
+        self.__window.close()
+        self.__window = win
+        
+        return self.__show_window(win)
     
-    @window.deleter
-    def window(self):
-        self.__window = None
-        self.__do_load = False
+    def chain_window(self, win, priority=PRIORITY_NONE):
+        logger.debug("Chaining window %s with priority %s", win, priority)
+        if not (self.PRIORITY_MIN <= priority <= self.PRIORITY_MAX):
+            raise ValueError("Invalid priority value {}".format(priority))
+        
+        if self.__started:
+            raise ValueError("Application already started cannot chain window")
+        
+        self.__window_chain[priority].append(win)
+        return win
+
+    def show_next(self):
+        logger.debug("Showing next window...")
+        try:
+            win = self.windows.next()
+        except StopIteration:
+            logger.debug("The next window is the main window")
+            return self.__show_main_window()
+        else:
+            logger.debug("The next window is %s", win)
+            return self.__show_window(win)
+    
+    @property
+    def windows(self):
+        """
+        An iterator over the chained windows, ordered by priority.
+        """
+        for p in sorted(self.__window_chain.iterkeys()):
+            try:
+                win = self.__window_chain[p].pop(0)
+            except IndexError:
+                del self.__window_chain[p]
+                continue
+            else:
+                yield win
+    
+    def extend_default_main_window(self, extension):
+        self.extensions.append(extension)
     
     def init(self):
         logger.info('Platform: %s' % (sys.platform,))
@@ -37,25 +105,30 @@ class QtUIHandler(cbpos.BaseUIHandler):
         return True
     
     def start(self):
-        if not self.application:
+        if self.application is None:
             logger.critical('No QApplication set up.')
             return False
         
         if not self.__do_load:
+            logger.debug('Will not load the window.')
             return False
         
-        if self.__window is None:
-            self.show_default()
-        else:
-            logger.debug('Main Window is not the default: ' + repr(self.__window))
-            self.__window.show()
+        if cbpos.first_run:
+            logger.info("Follow me, I'll get you to the wizard.")
+            wizard = QtGui.QWidget()
+            self.chain_window(wizard, self.PRIORITY_FIRST_HIGH)
+        
+        self.__started = True
+        self.show_next()
+        
         return self.application.exec_()
     
-    def show_default(self):
+    def __load_default_main_window(self):
         logger.debug('Importing main window...')
-        
         from cbpos.mod.base.views import MainWindow as BaseMainWindow
+        
         if len(self.extensions) > 0:
+            logger.debug('Loading main window extensions...')
             exts = tuple(self.extensions + [BaseMainWindow])
             MainWindow = type('ExtendedMainWindow', exts, {})
             
@@ -70,23 +143,38 @@ class QtUIHandler(cbpos.BaseUIHandler):
                     pass
                 else:
                     MainWindow.addInit(init)
-                    del ext.init
+            #del MainWindow.init
         else:
-            MainWindow = BaseMainWindow
-            
             logger.debug('No extensions.')
+            MainWindow = BaseMainWindow
         
         logger.debug('Loading main window %s...', MainWindow.__name__)
-        self.window = MainWindow()
+        self.__window = MainWindow()
+    
+    def __show_main_window(self):
+        if self.__window is None:
+            self.__load_default_main_window()
+        else:
+            logger.debug('Main Window is not the default: ' + repr(self.__window))
         
+        return self.__show_window(self.__window)
+    
+    def __show_window(self, win):
+        """
+        Show the window properly.
+        Hold on to a reference to the window so that it does not get garbage
+        collected.
+        Handles the fullscreen configuration too.
+        """
+        self.__current_window = win
+        # Decide whether to show the window in fullscreen or not
         fullscreen = bool(cbpos.config['app', 'fullscreen'])
         if fullscreen:
-            self.window.showFullScreen()
+            win.showFullScreen()
         else:
-            self.window.showNormal()
-    
-    def extend_default(self, extension):
-        self.extensions.append(extension)
+            win.showNormal()
+        
+        return self.__current_window
 
 class MainWindowExtension(object):
     pass
