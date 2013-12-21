99506649d9b6ca893c09a75add0851191f952c64
Jad Kik <jadkik94@gmail.com>
2013-12-21 16:20:06 +0200

    Fix install_requires in setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,14 @@ setup(
       namespace_packages=['cbpos', 'cbpos.mod'],
       include_package_data=True,
       
+      install_requires=[
+            'sqlalchemy>=0.7, <1.0',
+            'PyDispatcher>=2.0.3, <3.0',
+            'ProxyTypes>=0.9, <1.0',
+            'Babel>=1.3, <2.0',
+            'PySide>=1.1.2,<2.0'
+      ],
+      
       author='Coinbox POS Team',
       author_email='coinboxpos@googlegroups.com',
       description='Coinbox POS core package',
